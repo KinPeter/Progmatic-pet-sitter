624630d053f4719f80cf2727f6ce6a0e9912e1a5
Gina Metzger <gina.metzger@gmail.com>
2019-07-18 16:58:26 +0200

    add search-page in app-routing-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FrontPageComponent } from './components/front-page/front-page.component';
+import {SearchPageComponent} from './components/search-page/search-page.component';
 
 
 const routes: Routes = [
-    { path: '', component: FrontPageComponent }
+    { path: '', component: FrontPageComponent },
+    { path: 'search-page', component: SearchPageComponent},
 ];
 
 @NgModule({
