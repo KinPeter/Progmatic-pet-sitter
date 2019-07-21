1e0c26dfa0f016de2a4f594e90f62d92aba01926
Gina Metzger <gina.metzger@gmail.com>
2019-07-21 14:33:44 +0200

    post-code validator in main-search and field-validator.service

--- src/app/components/front-page/main-search/main-search.component.ts
+++ src/app/components/front-page/main-search/main-search.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { SearchData, PetType, PlaceOfService, KeyValue} from '../../../interfaces/search-data';
 import { SearchDataTransferService} from '../../../services/search-data-transfer.service'
 import { PettypeService} from '../../../services/pettype.service';
 import { ServicePlaceService} from '../../../services/service-place.service';
+import { FieldValidatorService } from 'src/app/services/field-validator.service';
 
 @Component({
     selector: 'app-main-search',
@@ -16,8 +17,9 @@ export class MainSearchComponent implements OnInit {
   searchData: SearchData
   petType: KeyValue[];
   sercivePlaceType: KeyValue[];
+  isPostcodeValid: boolean;
 
-  constructor(private router: Router, private searchDataTransferService: SearchDataTransferService, private pettypeService: PettypeService, private servicePlaceService: ServicePlaceService) {
+  constructor(private router: Router, private searchDataTransferService: SearchDataTransferService, private pettypeService: PettypeService, private servicePlaceService: ServicePlaceService, private validator: FieldValidatorService) {
     this.currentSlide = 0;
 
     this.searchData = {
@@ -29,6 +31,8 @@ export class MainSearchComponent implements OnInit {
 
     this.petType = this.pettypeService.getPetTypeArray();
     this.sercivePlaceType = this.servicePlaceService.getServicePlaceTypeArray();
+
+    this.isPostcodeValid = true;
   }
 
 
@@ -36,13 +40,12 @@ export class MainSearchComponent implements OnInit {
       setInterval(() => {
         this.currentSlide = (this.currentSlide + 1) % 3;
       }, 4000);
-
     }
 
-  store (): void {
-   this.searchDataTransferService.searchData = Object.assign({}, this.searchData);
-  }
-
+    store (): void {
+      this.isPostcodeValid = this.validator.validatePostcode(this.searchData.postcode);
+      this.searchDataTransferService.searchData = Object.assign({}, this.searchData);
+    }
 
 
 }
--- src/app/services/field-validator.service.ts
+++ src/app/services/field-validator.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+//import {SearchData} from '../interfaces/search-data'
 
 @Injectable({
     providedIn: 'root'
@@ -7,9 +8,16 @@ export class FieldValidatorService {
 
     private emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; //tslint:disable-line
 
+
     constructor() { }
 
     validateEmail(emailAddress: string): boolean {
         return this.emailRegex.test(emailAddress);
     }
+
+    validatePostcode(postcode: number): boolean {
+      return ! isNaN(parseInt('' + postcode, 10))
+    && /^[0-9]{4}\d*$/.test('' + postcode)
+    }
+
 }
