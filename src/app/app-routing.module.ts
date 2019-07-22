739189d862dce119be8b1a17446dee2b213f80d2
Gina Metzger <gina.metzger@gmail.com>
2019-07-22 14:17:16 +0200

    my-profile-page in app-routing-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { FrontPageComponent } from './components/front-page/front-page.component';
 import {SearchPageComponent} from './components/search-page/search-page.component';
 import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
+import { MyProfilePageComponent } from './components/my-profile-page/my-profile-page.component';
 
 
 const routes: Routes = [
     { path: '', component: FrontPageComponent, pathMatch: 'full' },
     { path: 'search-page', component: SearchPageComponent},
     { path: 'registration-page', component: RegistrationPageComponent},
+    { path: 'my-profile-page', component: MyProfilePageComponent},
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
