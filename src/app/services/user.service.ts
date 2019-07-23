24d79ec41c1d2343a5225b9d7d60663d2e2a6f86
Gina Metzger <gina.metzger@gmail.com>
2019-07-23 10:12:29 +0200

    modifyUser method in user.service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../interfaces/user';
+import { UserDTO } from '../interfaces/user-dto';
+import { UserError } from '../errors/user-error';
 
 @Injectable({
     providedIn: 'root'
 })
 export class UserService {
 
-    constructor(private http: HttpClient) { }
+    private readonly URL = '/my-profile-page';
+
+    constructor(private http: HttpClient) {}
+
+    private transformUserDTO(serverData: UserDTO ): User[]{
+      if (!serverData.success){
+        throw new UserError(serverData['error-infos']);
+      }
+      return serverData.users;
+    }
 
     // register(user: User) {
     //     return this.http.post(`${config.apiUrl}/users/register`, user);
@@ -16,5 +27,10 @@ export class UserService {
     // delete(id: number) {
     //     return this.http.delete(`${config.apiUrl}/users/${id}`);
     // }
+
+    modifyUser (user: User): Promise<User []> {
+    return this.http.put(this.URL + '?id=' + user.id, {user}, {withCredentials: true})
+    .toPromise().then(this.transformUserDTO);
+    }
 
 }
