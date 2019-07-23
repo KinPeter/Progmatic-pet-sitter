1e82d36376a406f4418b8c5d4109f8871a2311f2
Gina Metzger <gina.metzger@gmail.com>
2019-07-23 11:53:47 +0200

    field-validator in my-profile page, name validator in field-validator.service

--- src/app/components/my-profile-page/my-profile-page.component.ts
+++ src/app/components/my-profile-page/my-profile-page.component.ts
@@ -4,6 +4,7 @@ import {UserService} from '../../services/user.service';
 import { PettypeService } from '../../services/pettype.service';
 import { ServicePlaceService } from '../../services/service-place.service';
 import { SearchData, PetType, PlaceOfService, KeyValue} from '../../interfaces/search-data';
+import { FieldValidatorService } from 'src/app/services/field-validator.service';
 
 @Component({
     selector: 'app-my-profile-page',
@@ -14,16 +15,20 @@ export class MyProfilePageComponent implements OnInit {
 
     @Input()
     user: User;
+
     errors: string[];
     showNetworkAlert: boolean;
     petType: KeyValue[];
     sercivePlaceType: KeyValue[];
 
-    constructor(private pettypeService: PettypeService, private servicePlaceService: ServicePlaceService, private userService: UserService) {
+    constructor(private pettypeService: PettypeService, private servicePlaceService: ServicePlaceService,
+      private userService: UserService, private validator: FieldValidatorService) {
       this.errors = [];
       this.showNetworkAlert = false;
       this.petType = this.pettypeService.getPetTypeArray();
       this.sercivePlaceType = this.servicePlaceService.getServicePlaceTypeArray();
+
+    //  this.isPostcodeValid = true;
 
     }
 
