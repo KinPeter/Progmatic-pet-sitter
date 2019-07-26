8e622af7b9d1593261d4a08bc0a3dbb2f549c3c4
Peter Kin <kinpeter85@gmail.com>
2019-07-26 16:33:06 +0200

    add e2e test basic site navigation

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,41 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
-  }
+    navigateTo(): Promise<any> {
+        return browser.get(browser.baseUrl) as Promise<any>;
+    }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
-  }
+    getHeaderElement(): ElementFinder {
+        return element(by.css('app-header'));
+    }
+    getFrontPageElement(): ElementFinder {
+        return element(by.tagName('app-front-page'));
+    }
+    getFooterElement(): ElementFinder {
+        return element(by.tagName('app-footer'));
+    }
+    getLoginModalElement(): ElementFinder {
+        return element(by.tagName('app-login-modal'));
+    }
+    getRegistrationPageElement(): ElementFinder {
+        return element(by.tagName('app-registration-page'));
+    }
+    getLoginButton(): ElementFinder {
+        return element(by.css('.my-head-info .btn-success:first-of-type'));
+    }
+    getRegisterButton(): ElementFinder {
+        return element(by.css('.my-head-info .btn-success:last-of-type'));
+    }
+    getSearchNavbarButton(): ElementFinder {
+        return element(by.css('header ul li:nth-child(3) a'));
+    }
+    getSearchPageElement(): ElementFinder {
+        return element(by.tagName('app-search-page'));
+    }
+    getWannabeNavbarButton(): ElementFinder {
+        return element(by.css('header ul li:nth-child(4) a'));
+    }
+    getWannabePageElement(): ElementFinder {
+        return element(by.tagName('app-wannabe-sitter-page'));
+    }
 }
