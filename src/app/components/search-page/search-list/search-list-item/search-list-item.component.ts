8e9f7669310be162174a7e6255e109dac33b385b
Zsani <zsanettfrona@gmail.com>
2019-07-31 09:36:13 +0200

    search-list-item-heroku

--- src/app/components/search-page/search-list/search-list-item/search-list-item.component.ts
+++ src/app/components/search-page/search-list/search-list-item/search-list-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { SearchedSitter } from 'src/app/interfaces/searchedSitter';
 import { SearchDataTransferService } from 'src/app/services/search-data-transfer.service';
+import { herokuURL } from 'src/app/app-settings';
 
 @Component({
   selector: '[app-search-list-item]',
@@ -11,11 +12,14 @@ export class SearchListItemComponent implements OnInit {
 
   @Input()
   sitter: SearchedSitter;
-  private imgUrl: string;
+  private imgUrl: any;
 
-  constructor(private searchDataService: SearchDataTransferService) { }
+  constructor(private searchDataService: SearchDataTransferService) {
+    this.imgUrl = herokuURL + "/user/" + this.sitter.id + "/image";
+  }
 
   ngOnInit() {
+    console.log(this.imgUrl);
       if(this.imgUrl === ''){
           this.imgUrl = 'assets/images/defaultAvatar.png';
       }
